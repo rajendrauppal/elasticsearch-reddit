7c2c081e0eb6064be6982400f7be199c646080b4
rajendrauppal <rajen.iitd@gmail.com>
2015-05-18 00:56:06 +0530

    Started writing ES Reddit Python app

---
+++ elasticsearch_reddit.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+"""
+"""
+
+
+__author__ = 'Rajendra Kumar Uppal'
+
+
+import elasticsearch as es
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
