012d60c7e5856b5581785ac7f2466d800a40ab92
rajendrauppal <rajen.iitd@gmail.com>
2015-05-18 20:40:06 +0530

    Implementing elasticsearch reddit app

--- elasticsearch_reddit.py
+++ elasticsearch_reddit.py
@@ -3,17 +3,32 @@
 
 
 """
+Python application to fetch, store and offline search Reddit data.
 """
 
 
 __author__ = 'Rajendra Kumar Uppal'
 
 
-import elasticsearch as es
+import requests
+from elasticsearch import Elasticsearch
 
 
 def main():
-    pass
+    es = Elasticsearch()
+    
+    # get top 100 IAMA posts of all time
+    response = requests.get("http://api.reddit.com/r/iama/top/?t=all&limit=1", headers={"User-Agent":"TrackMaven"})
+    fields = ['title', 'selftext', 'author', 'score', 'ups', 'downs', 'num_comments', 'url', 'created']
+    
+    # loop through the response and add each data dictionary to reddit index
+    for i, iama in enumerate(response.json()['data']['children']):
+        content = iama['data']
+        doc = {}
+        for field in fields:
+            doc[field] = content[field]
+        print doc
+        es.index(index='reddit', doc_type='iama', body=doc)
 
 
 if __name__ == '__main__':
