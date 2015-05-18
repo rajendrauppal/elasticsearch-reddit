9e59469d09e4265862d9b673cdcb18175762b0b6
rajendrauppal <rajen.iitd@gmail.com>
2015-05-19 00:13:14 +0530

    Implementing elasticsearch reddit app

--- elasticsearch_reddit.py
+++ elasticsearch_reddit.py
@@ -4,10 +4,23 @@
 
 """
 Python application to fetch, store and offline search Reddit data.
+Requirements:
+1. Python 2.7+
+2. requests (pip install requests)
+3. elasticsearch (pip install elasticsearch)
+4. ElasticSearch server running
+(downlaod from https://www.elastic.co/downloads/elasticsearch, install and start)
 """
 
 
 __author__ = 'Rajendra Kumar Uppal'
+__copyright__ = "Copyright 2015, Rajendra Kumar Uppal"
+__credits__ = ["Fletcher Heisler", "Rajendra Kumar Uppal"]
+__license__ = "MIT"
+__version__ = "0.1"
+__maintainer__ = "Rajendra Kumar Uppal"
+__email__ = "rajen.iitd@gmail.com"
+__status__ = "Production"
 
 
 import requests
@@ -18,8 +31,10 @@ def main():
     es = Elasticsearch()
     
     # get top 100 IAMA posts of all time
-    response = requests.get("http://api.reddit.com/r/iama/top/?t=all&limit=1", headers={"User-Agent":"TrackMaven"})
-    fields = ['title', 'selftext', 'author', 'score', 'ups', 'downs', 'num_comments', 'url', 'created']
+    response = requests.get("http://api.reddit.com/r/iama/top/?t=all&limit=1", 
+        headers={"User-Agent":"TrackMaven"})
+    fields = ['title', 'selftext', 'author', 'score', 'ups', 'downs', 
+        'num_comments', 'url', 'created']
     
     # loop through the response and add each data dictionary to reddit index
     for i, iama in enumerate(response.json()['data']['children']):
